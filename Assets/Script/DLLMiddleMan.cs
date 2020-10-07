29672f3106e2da9f0a80a3852ebc3bd1babc45fe
Anthony <anth19800@hotmail.com>
2020-10-07 19:02:42 -0400

    [Add] More stuff

---
+++ Assets/Script/DLLMiddleMan.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class DLLMiddleMan : MonoBehaviour
+{
+    const string DllName = "NickTut";
+
+    [DllImport(DllName)]
+    private static extern void ResetLogger();
+
+    [DllImport(DllName)]
+    private static extern void SaveCheckpointTime(float RTBC);
+
+    [DllImport(DllName)]
+    private static extern float GetTotalTime();
+
+    [DllImport(DllName)]
+    private static extern float GetCheckpointTime(int index);
+
+    [DllImport(DllName)]
+    private static extern int GetNumCheckpoints();
+
+
+
+    public void SaveTimeTest(float checkpointTime)
+    {
+        SaveCheckpointTime(checkpointTime);
+    }
+
+    public float LoadTimeTest(int index)
+    {
+        if (index < 0 || index >= GetNumCheckpoints())
+            return -1.0f;
+        return GetCheckpointTime(index);
+    }
+
+    public float LoadTotalTimeTest()
+    {
+        return GetTotalTime();
+    }
+
+    public void ResetLoggerTest()
+    {
+        ResetLogger();
+    }
+
+    float lastTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float currentTime = Time.time;
+            float checkpointTime = currentTime - lastTime;
+            lastTime = currentTime;
+            SaveTimeTest(checkpointTime);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            {
+                Debug.Log(LoadTimeTest(i));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(LoadTotalTimeTest());
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetLoggerTest();
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    Vector3 lastMouse = new Vector3();
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            move(gameObject.transform.forward);
+        else if (Input.GetKey(KeyCode.S))
+            move(-gameObject.transform.forward);
+        if (Input.GetKey(KeyCode.A))
+            move(-gameObject.transform.right);
+        if (Input.GetKey(KeyCode.D))
+            move(gameObject.transform.right);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.rotation = Quaternion.AngleAxis(-500.0f * Time.deltaTime, Vector3.up) * transform.rotation;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.rotation = Quaternion.AngleAxis(500.0f * Time.deltaTime, Vector3.up) * transform.rotation;
+    }
+
+    void move(Vector3 direction)
+    {
+        gameObject.transform.position = gameObject.transform.position + (direction * moveSpeed * Time.deltaTime);
+    }
+
+
+
+}
